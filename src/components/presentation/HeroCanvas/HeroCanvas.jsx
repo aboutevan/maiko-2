28af00ed50bd48711396a24401086539762bf2de
Evan Salzbrenner <hello@aboutevan.com>
2017-04-07 06:52:15 -0700

    more shit

--- src/components/presentation/HeroCanvas/HeroCanvas.jsx
+++ src/components/presentation/HeroCanvas/HeroCanvas.jsx
@@ -1,14 +1,41 @@
 /* eslint-disable */
 import React, { Component } from 'react';
 import { scaleSpriteToCanvas, spritePaths, positionSprite, spriteOnMouseMove } from 'core/js/pixi.utils';
-import * as pixi from 'pixi.js';
+import * as PIXI from 'pixi.js';
 import * as ass from 'img/a-base.png';
 
-// LOADING BAR https://github.com/kittykatattack/learningpixi/blob/master/README.md
+// LOADING BAR https://github.com/kittykatattack/learningPIXI/blob/master/README.md
 
 export default class HeroCanvas extends Component {
   constructor(props) {
     super(props);
+
+    this.pixiApp = new PIXI.Application(window.innerWidth, window.innerHeight);
+    this.renderer = this.pixiApp.renderer;
+    this.stage = this.pixiApp.stage;
+    this.bgContainer = new PIXI.Container();
+    this.layerAll = new PIXI.Container();
+    this.layerRight = new PIXI.Container();
+    this.layerLeft = new PIXI.Container();
+
+    this.renderer.view.style.position = 'absolute';
+    this.renderer.view.style.display = 'block';
+    this.renderer.view.style.minHeight = 697;
+    this.renderer.autoResize = true;
+
+    this.sprites = spritePaths();
+
+    this.addContainersToStage();
+    this.addPathstoLoader();
+    this.initPixiLoader();
+
+    // const sprites = spritePaths();
+
+    // const bunny = PIXI.Sprite.fromImage(sprites.abase);
+
+    // bunny.anchor.set(0.7);
+
+    // this.stage.addChild(bunny);
 
 
     this.settings = {
@@ -36,49 +63,74 @@ export default class HeroCanvas extends Component {
     // use scripts meant for use in browser in our server-side
     // apps - the scripts won't be passed to our Node server and
     // result in undefined errors - not the greatest solution
-    // const pixi = require('pixi.js');
-    // pixi.utils.skipHello(); // disable pixi banner in console
+    // const PIXI = require('PIXI.js');
+    // PIXI.utils.skipHello(); // disable PIXI banner in console
 
     // // define all cached variables
-    // this.pixiApp = new pixi.Application(window.innerWidth, window.innerHeight);
+    // this.PIXIApp = new PIXI.Application(window.innerWidth, window.innerHeight);
 
     // const sprites = spritePaths();
 
-    // const bunny = pixi.Sprite.fromImage(sprites.abase);
+    // const bunny = PIXI.Sprite.fromImage(sprites.abase);
 
     // bunny.anchor.set(0.5);
 
     // app.stage.addChild(bunny);
 
     // append it to component
-    // pixi generates its own canvas element, which has to be
+    // PIXI generates its own canvas element, which has to be
     // appended to dom.  Here, this.component is assigned a reference
     // to the mounted component, using the 'ref' attribute in the
     // render method below. So, we are creating and appending our canvas
     // in one component, instead creating a component just to append to
-    // another.  If we weren't using pixi, we could just render a canvas
+    // another.  If we weren't using PIXI, we could just render a canvas
     // element directly
-    // 
-    
-    this.pixiApp = new pixi.Application(window.innerWidth, window.innerHeight);
-    this.renderer = this.pixiApp.renderer;
-    this.stage = this.pixiApp.stage;
-    this.bgContainer = new pixi.Container();
-
-    const sprites = spritePaths();
-
-    const bunny = pixi.Sprite.fromImage(sprites.abase);
-
-    bunny.anchor.set(0.2);
-
-    this.stage.addChild(bunny);
 
     this.component.appendChild(this.renderer.view);
 
-    // manipulate the pixi canvas
+    // manipulate the PIXI canvas
 
   }
 
+
+  addContainersToStage() {
+    this.layerAll.addChild(this.layerRight);
+    this.layerAll.addChild(this.layerLeft);
+    [
+      this.bgContainer,
+      this.layerAll,
+    ].map(container => this.stage.addChild(container));
+  }
+
+  canvasSetup() {
+    const resources = PIXI.loader.resources;
+
+    this.layerRightChildren = [
+      new PIXI.Sprite(resources[this.sprites.abase].texture)
+    ]
+
+    this.layerRightChildren.map(child => this.layerRight.addChild(child));
+  }
+
+  addPathstoLoader() {
+    if (process.env.NODE_ENV !== 'production') {
+      PIXI.loader.reset();
+    }
+
+    for (const path of Object.keys(this.sprites)) {
+      PIXI.loader.add(this.sprites[path]);
+    }
+
+    console.log(PIXI.loader);
+
+    // this.sprites.map(path => PIXI.loader.add(path));
+  }
+
+  initPixiLoader() {
+    const setup = this.canvasSetup;
+    PIXI.loader
+      .load(setup.bind(this));
+  }
 
 
 
