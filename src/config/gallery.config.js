59010517c0db805dd3ac13f883b823661b3456be
Evan Salzbrenner <hello@aboutevan.com>
2017-05-08 07:41:24 -0700

    portimage layout

--- src/config/gallery.config.js
+++ src/config/gallery.config.js
@@ -1,18 +1,62 @@
 export default [
   {
-    url: 'gallery-1.jpg',
-    title: 'scarface woman',
+    url: 'gallery-3.jpg',
+    title: 'hanging body',
   },
   {
     url: 'gallery-2.jpg',
     title: 'scarface 1',
   },
   {
-    url: 'gallery-3.jpg',
-    title: 'scarface 2',
+    url: 'gallery-1.jpg',
+    title: 'scarface woman',
+  },
+  {
+    url: 'gallery-15.jpg',
+    title: 'sculpture',
+  },
+  {
+    url: 'gallery-13.jpg',
+    title: 'drawing',
+  },
+  {
+    url: 'gallery-6.jpg',
+    title: 'sculpture',
+  },
+  {
+    url: 'gallery-11.jpg',
+    title: 'clown',
   },
   {
     url: 'gallery-4.jpg',
-    title: 'scarface 3',
+    title: 'drawing clown',
   },
+  {
+    url: 'gallery-10.jpg',
+    title: 'clown',
+  },
+  {
+    url: 'gallery-9.jpg',
+    title: 'piggy',
+  },
+  {
+    url: 'gallery-5.jpg',
+    title: 'hand',
+  },
+  {
+    url: 'gallery-14.jpg',
+    title: 'sculpture',
+  },
+  {
+    url: 'gallery-7.jpg',
+    title: 'sculpture',
+  },
+  {
+    url: 'gallery-12.jpg',
+    title: 'drawing',
+  },
+  // {
+  //   url: 'gallery-8.jpg',
+  //   title: 'sculpture',
+  // },
 ];
